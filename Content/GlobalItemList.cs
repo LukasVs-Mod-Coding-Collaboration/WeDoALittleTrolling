ea76f800d5d15aad01c1aeabf55dcb2d5b7efccb
LukasV-Coding <lukas@lukasvogl.de>
2022-09-24 22:08:07 +0200

    Undid Phantasm's base damage nerf
    Vanilla item Changes:
    -Phantasm:
    
    Damage 50 -> 70

---
+++ Content/GlobalItemList.cs
@@ -0,0 +1,23 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Microsoft.Xna.Framework;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.GameContent.Creative;
+using static Humanizer.In;
+using static Terraria.ModLoader.PlayerDrawLayer;
+
+namespace WeDoALittleTrolling.Content.Items
+{
+    internal class GlobalItemList : GlobalItem
+    {
+
+        public override void SetDefaults(Item item)
+        {
+            if (item.type == ItemID.Phantasm) item.damage = 70;
+        }
+
+
+    }
+}
