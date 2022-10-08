a93b96fe22999341dda5ddf0f4a4d6587471a175
LukasV-Coding <lukas@lukasvogl.de>
2022-10-08 17:38:47 +0200

    Infinite rockets seem bugged
    e

---
+++ Content/Items/InfiniteRocketAmmo.cs
@@ -0,0 +1,53 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Microsoft.Xna.Framework;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.GameContent.Creative;
+using static Humanizer.In;
+using static Terraria.ModLoader.PlayerDrawLayer;
+using System;
+
+
+namespace WeDoALittleTrolling.Content.Items
+{
+    internal class InfiniteRocketAmmo : ModItem
+    {
+
+
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Infinite Rocket I");
+            Tooltip.SetDefault("It seems to multiply as you load it into your weapon...");
+        }
+
+
+        public override void SetDefaults()
+        {
+            Item.width = 46;
+            Item.height = 50;
+
+            Item.value = Item.buyPrice(copper: 1);
+            Item.maxStack = 1;
+
+            Item.damage = 40;
+            Item.DamageType = DamageClass.Ranged;
+
+            Item.rare = ItemRarityID.Red;
+            Item.shoot = ProjectileID.RocketI;
+            Item.ammo = AmmoID.Rocket;
+
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+              .AddTile(TileID.CrystalBall)
+              .AddIngredient(ItemID.RocketI, 3000)
+              .AddIngredient(ItemID.Ectoplasm, 15)
+              .Register();
+        }
+    }
+}
