20a5e7bdbd178891c00b1285e6d3af6f6b736923
LukasV-Coding <lukas@lukasvogl.de>
2023-05-07 21:37:47 +0200

    Prefixes!
    Added the extremely funny and extremely overpowered colossal reforge, which takes 30% of your dps off in return for insane size and also some strong knockback

---
+++ Content/Prefixes/Colossal.cs
@@ -0,0 +1,54 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Microsoft.Xna.Framework;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.GameContent.Creative;
+using WeDoALittleTrolling.Content.Projectiles;
+using static Humanizer.In;
+using static Terraria.ModLoader.PlayerDrawLayer;
+using System;
+
+namespace WeDoALittleTrolling.Content.Prefixes
+{
+
+    public class Colossal : ModPrefix
+    {
+
+        //public virtual float Power => 1f;
+        // Change your category this way, defaults to PrefixCategory.Custom. Affects which items can get this prefix.
+        public override PrefixCategory Category => PrefixCategory.Melee;
+        public override float RollChance(Item item)
+        {
+            return 1f;
+        }
+
+        // Determines if it can roll at all.
+        public override bool CanRoll(Item item)
+        {
+            return true;
+        }
+
+        // Use this function to modify these stats for items which have this prefix:
+        // Damage Multiplier, Knockback Multiplier, Use Time Multiplier, Scale Multiplier (Size), Shoot Speed Multiplier, Mana Multiplier (Mana cost), Crit Bonus.
+        public override void SetStats(ref float damageMult, ref float knockbackMult, ref float useTimeMult, ref float scaleMult, ref float shootSpeedMult, ref float manaMult, ref int critBonus)
+        {
+            scaleMult *= 2.0f; // Add + x * Power to allow for other mods to change effectiveness of modifier
+            useTimeMult *= 1.3f;
+            knockbackMult *= 1.25f;
+        }
+
+        // Modify the cost of items with this modifier with this function.
+        public override void ModifyValue(ref float valueMult)
+        {
+            valueMult *= 1f + 0.05f;
+        }
+
+        // This is used to modify most other stats of items which have this modifier.
+        public override void Apply(Item item)
+        {
+            //
+        }
+    }
+}
