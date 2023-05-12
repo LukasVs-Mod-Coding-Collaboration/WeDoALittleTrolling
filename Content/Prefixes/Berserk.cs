60b0cd5fbe92c41f87f4c47e1b4a28e1114814fa
LukasV-Coding <lukas@lukasvogl.de>
2023-05-12 17:46:14 +0200

    Berserk Reforge (Not broken but funny)

---
+++ Content/Prefixes/Berserk.cs
@@ -0,0 +1,47 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Microsoft.Xna.Framework;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.GameContent.Creative;
+using WeDoALittleTrolling.Content.Projectiles;
+using static Humanizer.In;
+using static Terraria.ModLoader.PlayerDrawLayer;
+using System;
+
+namespace WeDoALittleTrolling.Content.Prefixes
+{
+
+    public class Berserk : ModPrefix
+    {
+
+        public override PrefixCategory Category => PrefixCategory.Melee;
+        public override float RollChance(Item item)
+        {
+            return 1.5f;
+        }
+
+        public override bool CanRoll(Item item)
+        {
+            return true;
+        }
+
+        public override void SetStats(ref float damageMult, ref float knockbackMult, ref float useTimeMult, ref float scaleMult, ref float shootSpeedMult, ref float manaMult, ref int critBonus)
+        {
+            damageMult *= 0.7f;
+            useTimeMult *= 0.75f;
+            knockbackMult *= 1.25f;
+        }
+
+        public override void ModifyValue(ref float valueMult)
+        {
+            valueMult *= 1f + 0.05f;
+        }
+
+        public override void Apply(Item item)
+        {
+            //
+        }
+    }
+}
