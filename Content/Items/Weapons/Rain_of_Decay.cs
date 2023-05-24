6737810401c3e64410dc5c4c8d52bb543a5d9ced
LukasV-Coding <lukas@lukasvogl.de>
2023-05-24 20:28:17 +0200

    Rain of Decay Attack Mode 1 Fully Implemented
    Attack mode 1 works now I guess

--- Content/Items/Weapons/Rain_of_Decay.cs
+++ Content/Items/Weapons/Rain_of_Decay.cs
@@ -26,42 +26,72 @@ namespace WeDoALittleTrolling.Content.Items.Weapons
             Item.height = 142;
             Item.rare = ItemRarityID.Expert;
 
-            Item.useTime = 25;
-            Item.useAnimation = 25;
+            Item.useTime = 20;
+            Item.useAnimation = 20;
             Item.useStyle = ItemUseStyleID.Shoot;
             Item.UseSound = SoundID.Item5;
             Item.DamageType = DamageClass.Ranged;
-            Item.damage = 60;
-            Item.knockBack = 0.5f;
+            Item.damage = 36;
+            Item.knockBack = 1.25f;
             Item.noMelee = true;
             Item.crit = 0;
-            Item.shoot = ProjectileID.CursedArrow;
-            Item.shootSpeed = 10.0f;
+            Item.shoot = ProjectileID.WoodenArrowFriendly;
+            Item.shootSpeed = 13.0f;
+            Item.useAmmo = AmmoID.Arrow;
             Item.autoReuse = true;
+
 
         }
 
-        /*
-          
-         
-        public override bool AltFunctionUse(Player player)
+
+        public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
         {
+            if (type == ProjectileID.WoodenArrowFriendly)
+            {
+                type = ProjectileID.CursedArrow;
+            }
+        }
+
+
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            float numberProjectiles = 4;
+            float rotation = MathHelper.ToRadians(5);
+            position += Vector2.Normalize(velocity) * 5f;
+            for (int i = 0; i < numberProjectiles; i++)
+            {
+                Vector2 burstArrowSpeed = velocity.RotatedBy(MathHelper.Lerp(-rotation, rotation, i / (numberProjectiles)));
+                Projectile.NewProjectile(source, position, burstArrowSpeed, type, damage, knockback, player.whoAmI);
+            }
             return false;
         }
 
 
-        */
-
-
-        // public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback) { }
-
-
-        /*
-        public override Vector2? HoldoutOffset()
+        public override void AddRecipes()
         {
-            return new Vector2(-36.5f, -5.0f);
+            CreateRecipe()
+              .AddTile(TileID.HeavyWorkBench)
+              .AddIngredient(ItemID.CursedFlame, 25)
+              .AddIngredient(ItemID.AshWood, 20)
+              .AddIngredient(ItemID.IronBar, 15)
+              .AddIngredient(ItemID.SoulofNight, 10)
+              .AddIngredient(ItemID.SoulofSight, 5)
+              .AddIngredient(ItemID.JungleSpores, 5)
+              .AddIngredient(ItemID.WhiteString, 1)
+              .Register();
+
+
+            CreateRecipe()
+             .AddTile(TileID.HeavyWorkBench)
+             .AddIngredient(ItemID.Ichor, 25)
+             .AddIngredient(ItemID.AshWood, 20)
+             .AddIngredient(ItemID.IronBar, 15)
+             .AddIngredient(ItemID.SoulofNight, 10)
+             .AddIngredient(ItemID.SoulofSight, 5)
+             .AddIngredient(ItemID.JungleSpores, 5)
+             .AddIngredient(ItemID.WhiteString, 1)
+             .Register();
         }
-        */
 
     }
 }
