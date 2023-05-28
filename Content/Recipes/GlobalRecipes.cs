c730d8f7e9e142b4958fcaae1158ae1f275d93b8
DarkWav <simon.vogl@gmx.net>
2023-05-28 19:23:09 +0200

    More Balance...

--- Content/Recipes/GlobalRecipes.cs
+++ Content/Recipes/GlobalRecipes.cs
@@ -176,6 +176,275 @@ namespace WeDoALittleTrolling.Content.Recipes
                     recipe.RemoveIngredient(ItemID.TinBar);
                     recipe.AddIngredient(ItemID.TinBar, 8);
                 }
+
+                //Cobalt Armor
+
+                if(recipe.TryGetResult(ItemID.CobaltHat, out Item CobaltHat))
+                {
+                    recipe.RemoveIngredient(ItemID.CobaltBar);
+                    recipe.AddIngredient(ItemID.CobaltBar, 10);
+                }
+                if(recipe.TryGetResult(ItemID.CobaltHelmet, out Item CobaltHelmet))
+                {
+                    recipe.RemoveIngredient(ItemID.CobaltBar);
+                    recipe.AddIngredient(ItemID.CobaltBar, 10);
+                }
+                if(recipe.TryGetResult(ItemID.CobaltMask, out Item CobaltMask))
+                {
+                    recipe.RemoveIngredient(ItemID.CobaltBar);
+                    recipe.AddIngredient(ItemID.CobaltBar, 10);
+                }
+                if(recipe.TryGetResult(ItemID.CobaltBreastplate, out Item CobaltBreastplate))
+                {
+                    recipe.RemoveIngredient(ItemID.CobaltBar);
+                    recipe.AddIngredient(ItemID.CobaltBar, 15);
+                }
+                if(recipe.TryGetResult(ItemID.CobaltLeggings, out Item CobaltLeggings))
+                {
+                    recipe.RemoveIngredient(ItemID.CobaltBar);
+                    recipe.AddIngredient(ItemID.CobaltBar, 10);
+                }
+
+                //Palladium Armor
+
+                if(recipe.TryGetResult(ItemID.PalladiumHelmet, out Item PalladiumHelmet))
+                {
+                    recipe.RemoveIngredient(ItemID.PalladiumBar);
+                    recipe.AddIngredient(ItemID.PalladiumBar, 10);
+                }
+                if(recipe.TryGetResult(ItemID.PalladiumHeadgear, out Item PalladiumHeadgear))
+                {
+                    recipe.RemoveIngredient(ItemID.PalladiumBar);
+                    recipe.AddIngredient(ItemID.PalladiumBar, 10);
+                }
+                if(recipe.TryGetResult(ItemID.PalladiumMask, out Item PalladiumMask))
+                {
+                    recipe.RemoveIngredient(ItemID.PalladiumBar);
+                    recipe.AddIngredient(ItemID.PalladiumBar, 10);
+                }
+                if(recipe.TryGetResult(ItemID.PalladiumBreastplate, out Item PalladiumBreastplate))
+                {
+                    recipe.RemoveIngredient(ItemID.PalladiumBar);
+                    recipe.AddIngredient(ItemID.PalladiumBar, 15);
+                }
+                if(recipe.TryGetResult(ItemID.PalladiumLeggings, out Item PalladiumLeggings))
+                {
+                    recipe.RemoveIngredient(ItemID.PalladiumBar);
+                    recipe.AddIngredient(ItemID.PalladiumBar, 15);
+                }
+
+                //Mythril Armor
+
+                if(recipe.TryGetResult(ItemID.MythrilHat, out Item MythrilHat))
+                {
+                    recipe.RemoveIngredient(ItemID.MythrilBar);
+                    recipe.AddIngredient(ItemID.MythrilBar, 10);
+                }
+                if(recipe.TryGetResult(ItemID.MythrilHelmet, out Item MythrilHelmet))
+                {
+                    recipe.RemoveIngredient(ItemID.MythrilBar);
+                    recipe.AddIngredient(ItemID.MythrilBar, 10);
+                }
+                if(recipe.TryGetResult(ItemID.MythrilHood, out Item MythrilMask))
+                {
+                    recipe.RemoveIngredient(ItemID.MythrilBar);
+                    recipe.AddIngredient(ItemID.MythrilBar, 10);
+                }
+                if(recipe.TryGetResult(ItemID.MythrilChainmail, out Item MythrilChainmail))
+                {
+                    recipe.RemoveIngredient(ItemID.MythrilBar);
+                    recipe.AddIngredient(ItemID.MythrilBar, 15);
+                }
+                if(recipe.TryGetResult(ItemID.MythrilGreaves, out Item MythrilGreaves))
+                {
+                    recipe.RemoveIngredient(ItemID.MythrilBar);
+                    recipe.AddIngredient(ItemID.MythrilBar, 10);
+                }
+
+                //Orichalcum Armor
+
+                if(recipe.TryGetResult(ItemID.OrichalcumHelmet, out Item OrichalcumHelmet))
+                {
+                    recipe.RemoveIngredient(ItemID.OrichalcumBar);
+                    recipe.AddIngredient(ItemID.OrichalcumBar, 10);
+                }
+                if(recipe.TryGetResult(ItemID.OrichalcumHeadgear, out Item OrichalcumHeadgear))
+                {
+                    recipe.RemoveIngredient(ItemID.OrichalcumBar);
+                    recipe.AddIngredient(ItemID.OrichalcumBar, 10);
+                }
+                if(recipe.TryGetResult(ItemID.OrichalcumMask, out Item OrichalcumMask))
+                {
+                    recipe.RemoveIngredient(ItemID.OrichalcumBar);
+                    recipe.AddIngredient(ItemID.OrichalcumBar, 10);
+                }
+                if(recipe.TryGetResult(ItemID.OrichalcumBreastplate, out Item OrichalcumBreastplate))
+                {
+                    recipe.RemoveIngredient(ItemID.OrichalcumBar);
+                    recipe.AddIngredient(ItemID.OrichalcumBar, 15);
+                }
+                if(recipe.TryGetResult(ItemID.OrichalcumLeggings, out Item OrichalcumLeggings))
+                {
+                    recipe.RemoveIngredient(ItemID.OrichalcumBar);
+                    recipe.AddIngredient(ItemID.OrichalcumBar, 15);
+                }
+
+                //Adamantite Armor
+
+                if(recipe.TryGetResult(ItemID.AdamantiteHelmet, out Item AdamantiteHelmet))
+                {
+                    recipe.RemoveIngredient(ItemID.AdamantiteBar);
+                    recipe.AddIngredient(ItemID.AdamantiteBar, 10);
+                }
+                if(recipe.TryGetResult(ItemID.AdamantiteHeadgear, out Item AdamantiteHeadgear))
+                {
+                    recipe.RemoveIngredient(ItemID.AdamantiteBar);
+                    recipe.AddIngredient(ItemID.AdamantiteBar, 10);
+                }
+                if(recipe.TryGetResult(ItemID.AdamantiteMask, out Item AdamantiteMask))
+                {
+                    recipe.RemoveIngredient(ItemID.AdamantiteBar);
+                    recipe.AddIngredient(ItemID.AdamantiteBar, 10);
+                }
+                if(recipe.TryGetResult(ItemID.AdamantiteBreastplate, out Item AdamantiteBreastplate))
+                {
+                    recipe.RemoveIngredient(ItemID.AdamantiteBar);
+                    recipe.AddIngredient(ItemID.AdamantiteBar, 15);
+                }
+                if(recipe.TryGetResult(ItemID.AdamantiteLeggings, out Item AdamantiteLeggings))
+                {
+                    recipe.RemoveIngredient(ItemID.AdamantiteBar);
+                    recipe.AddIngredient(ItemID.AdamantiteBar, 15);
+                }
+
+                //Titanium Armor
+
+                if(recipe.TryGetResult(ItemID.TitaniumHelmet, out Item TitaniumHelmet))
+                {
+                    recipe.RemoveIngredient(ItemID.TitaniumBar);
+                    recipe.AddIngredient(ItemID.TitaniumBar, 15);
+                }
+                if(recipe.TryGetResult(ItemID.TitaniumHeadgear, out Item TitaniumHeadgear))
+                {
+                    recipe.RemoveIngredient(ItemID.TitaniumBar);
+                    recipe.AddIngredient(ItemID.TitaniumBar, 15);
+                }
+                if(recipe.TryGetResult(ItemID.TitaniumMask, out Item TitaniumMask))
+                {
+                    recipe.RemoveIngredient(ItemID.TitaniumBar);
+                    recipe.AddIngredient(ItemID.TitaniumBar, 15);
+                }
+                if(recipe.TryGetResult(ItemID.TitaniumBreastplate, out Item TitaniumBreastplate))
+                {
+                    recipe.RemoveIngredient(ItemID.TitaniumBar);
+                    recipe.AddIngredient(ItemID.TitaniumBar, 15);
+                }
+                if(recipe.TryGetResult(ItemID.TitaniumLeggings, out Item TitaniumLeggings))
+                {
+                    recipe.RemoveIngredient(ItemID.TitaniumBar);
+                    recipe.AddIngredient(ItemID.TitaniumBar, 15);
+                }
+
+                //Frost Armor
+
+                if(recipe.TryGetResult(ItemID.FrostHelmet, out Item FrostHelmet))
+                {
+                    if(recipe.TryGetIngredient(ItemID.AdamantiteBar, out Item AdamantiteBar_FrostHelmet))
+                    {
+                        recipe.RemoveIngredient(ItemID.AdamantiteBar);
+                        recipe.AddIngredient(ItemID.AdamantiteBar, 10);
+                    }
+                    if(recipe.TryGetIngredient(ItemID.TitaniumBar, out Item TitaniumBar_FrostHelmet))
+                    {
+                        recipe.RemoveIngredient(ItemID.TitaniumBar);
+                        recipe.AddIngredient(ItemID.TitaniumBar, 10);
+                    }
+                }
+                if(recipe.TryGetResult(ItemID.FrostBreastplate, out Item FrostBreastplate))
+                {
+                    if(recipe.TryGetIngredient(ItemID.AdamantiteBar, out Item AdamantiteBar_FrostBreastplate))
+                    {
+                        recipe.RemoveIngredient(ItemID.AdamantiteBar);
+                        recipe.AddIngredient(ItemID.AdamantiteBar, 15);
+                    }
+                    if(recipe.TryGetIngredient(ItemID.TitaniumBar, out Item TitaniumBar_FrostBreastplate))
+                    {
+                        recipe.RemoveIngredient(ItemID.TitaniumBar);
+                        recipe.AddIngredient(ItemID.TitaniumBar, 15);
+                    }
+                }
+                if(recipe.TryGetResult(ItemID.FrostLeggings, out Item FrostLeggings))
+                {
+                    if(recipe.TryGetIngredient(ItemID.AdamantiteBar, out Item AdamantiteBar_FrostLeggings))
+                    {
+                        recipe.RemoveIngredient(ItemID.AdamantiteBar);
+                        recipe.AddIngredient(ItemID.AdamantiteBar, 10);
+                    }
+                    if(recipe.TryGetIngredient(ItemID.TitaniumBar, out Item TitaniumBar_FrostLeggings))
+                    {
+                        recipe.RemoveIngredient(ItemID.TitaniumBar);
+                        recipe.AddIngredient(ItemID.TitaniumBar, 10);
+                    }
+                }
+
+                //Forbidden Armor
+
+                if(recipe.TryGetResult(ItemID.AncientBattleArmorHat, out Item AncientBattleArmorHat))
+                {
+                    if(recipe.TryGetIngredient(ItemID.AdamantiteBar, out Item AdamantiteBar_AncientBattleArmorHat))
+                    {
+                        recipe.RemoveIngredient(ItemID.AdamantiteBar);
+                        recipe.AddIngredient(ItemID.AdamantiteBar, 10);
+                    }
+                    if(recipe.TryGetIngredient(ItemID.TitaniumBar, out Item TitaniumBar_AncientBattleArmorHat))
+                    {
+                        recipe.RemoveIngredient(ItemID.TitaniumBar);
+                        recipe.AddIngredient(ItemID.TitaniumBar, 10);
+                    }
+                }
+                if(recipe.TryGetResult(ItemID.AncientBattleArmorShirt, out Item AncientBattleArmorShirt))
+                {
+                    if(recipe.TryGetIngredient(ItemID.AdamantiteBar, out Item AdamantiteBar_AncientBattleArmorShirt))
+                    {
+                        recipe.RemoveIngredient(ItemID.AdamantiteBar);
+                        recipe.AddIngredient(ItemID.AdamantiteBar, 15);
+                    }
+                    if(recipe.TryGetIngredient(ItemID.TitaniumBar, out Item TitaniumBar_AncientBattleArmorShirt))
+                    {
+                        recipe.RemoveIngredient(ItemID.TitaniumBar);
+                        recipe.AddIngredient(ItemID.TitaniumBar, 15);
+                    }
+                }
+                if(recipe.TryGetResult(ItemID.AncientBattleArmorPants, out Item AncientBattleArmorPants))
+                {
+                    if(recipe.TryGetIngredient(ItemID.AdamantiteBar, out Item AdamantiteBar_AncientBattleArmorPants))
+                    {
+                        recipe.RemoveIngredient(ItemID.AdamantiteBar);
+                        recipe.AddIngredient(ItemID.AdamantiteBar, 10);
+                    }
+                    if(recipe.TryGetIngredient(ItemID.TitaniumBar, out Item TitaniumBar_AncientBattleArmorPants))
+                    {
+                        recipe.RemoveIngredient(ItemID.TitaniumBar);
+                        recipe.AddIngredient(ItemID.TitaniumBar, 10);
+                    }
+                }
+
+                //Chlorophyte Bar
+
+                if(recipe.TryGetResult(ItemID.ChlorophyteBar, out Item ChlorophyteBar))
+                {
+                    recipe.RemoveIngredient(ItemID.ChlorophyteOre);
+                    recipe.AddIngredient(ItemID.ChlorophyteOre, 3);
+                }
+
+                //Chlorophyte Bullet
+
+                if(recipe.TryGetResult(ItemID.ChlorophyteBullet, out Item ChlorophyteBullet))
+                {
+                    recipe.RemoveIngredient(ItemID.MusketBall);
+                    recipe.AddIngredient(ItemID.MusketBall, 150);
+                    ChlorophyteBullet.stack = 150;
+                }
             }
         }
     }
