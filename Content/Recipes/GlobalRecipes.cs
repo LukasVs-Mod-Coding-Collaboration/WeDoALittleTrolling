474a23967510cb8b778aa14b79e297c63189e779
DarkWav <simon.vogl@gmx.net>
2023-05-28 17:43:16 +0200

    GlobalRecepies

--- Content/Recipes/GlobalRecipes.cs
+++ Content/Recipes/GlobalRecipes.cs
@@ -15,6 +15,7 @@ namespace WeDoALittleTrolling.Content.Recipes
 {
     internal class GlobalRecipes : ModSystem
     {
+        
         public override void AddRecipes()
         {
             /*
@@ -24,6 +25,44 @@ namespace WeDoALittleTrolling.Content.Recipes
             WDALT_SharkToothNecklace.AddRecipeGroup(RecipeGroupID.IronBar, 10);
             WDALT_SharkToothNecklace.Register();
             */
+        }
+
+        public override void PostAddRecipes()
+        {
+            for(int i = 0; i < Recipe.numRecipes; i++)
+            {
+                Recipe recipe = Main.recipe[i];
+                if(recipe.TryGetResult(ItemID.PlatinumHelmet, out Item PlatinumHelmet))
+                {
+                    recipe.RemoveIngredient(ItemID.PlatinumBar);
+                    recipe.AddIngredient(ItemID.PlatinumBar, 12);
+                }
+                if(recipe.TryGetResult(ItemID.PlatinumChainmail, out Item PlatinumChainmail))
+                {
+                    recipe.RemoveIngredient(ItemID.PlatinumBar);
+                    recipe.AddIngredient(ItemID.PlatinumBar, 22);
+                }
+                if(recipe.TryGetResult(ItemID.PlatinumGreaves, out Item PlatinumGreaves))
+                {
+                    recipe.RemoveIngredient(ItemID.PlatinumBar);
+                    recipe.AddIngredient(ItemID.PlatinumBar, 16);
+                }
+                if(recipe.TryGetResult(ItemID.GoldHelmet, out Item GoldHelmet))
+                {
+                    recipe.RemoveIngredient(ItemID.GoldBar);
+                    recipe.AddIngredient(ItemID.GoldBar, 12);
+                }
+                if(recipe.TryGetResult(ItemID.GoldChainmail, out Item GoldChainmail))
+                {
+                    recipe.RemoveIngredient(ItemID.GoldBar);
+                    recipe.AddIngredient(ItemID.GoldBar, 22);
+                }
+                if(recipe.TryGetResult(ItemID.GoldGreaves, out Item GoldGreaves))
+                {
+                    recipe.RemoveIngredient(ItemID.GoldBar);
+                    recipe.AddIngredient(ItemID.GoldBar, 16);
+                }
+            }
         }
     }
 }
