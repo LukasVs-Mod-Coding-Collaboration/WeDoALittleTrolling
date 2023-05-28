b01f816750ee78fea5f90eacc1cdf923b0671274
DarkWav <simon.vogl@gmx.net>
2023-05-28 18:06:33 +0200

    Pre Hardmode Armor Buffs

--- Content/Recipes/GlobalRecipes.cs
+++ Content/Recipes/GlobalRecipes.cs
@@ -32,6 +32,9 @@ namespace WeDoALittleTrolling.Content.Recipes
             for(int i = 0; i < Recipe.numRecipes; i++)
             {
                 Recipe recipe = Main.recipe[i];
+
+                //Platinum Armor
+
                 if(recipe.TryGetResult(ItemID.PlatinumHelmet, out Item PlatinumHelmet))
                 {
                     recipe.RemoveIngredient(ItemID.PlatinumBar);
@@ -47,6 +50,9 @@ namespace WeDoALittleTrolling.Content.Recipes
                     recipe.RemoveIngredient(ItemID.PlatinumBar);
                     recipe.AddIngredient(ItemID.PlatinumBar, 16);
                 }
+
+                //Gold Armor
+
                 if(recipe.TryGetResult(ItemID.GoldHelmet, out Item GoldHelmet))
                 {
                     recipe.RemoveIngredient(ItemID.GoldBar);
@@ -61,6 +67,114 @@ namespace WeDoALittleTrolling.Content.Recipes
                 {
                     recipe.RemoveIngredient(ItemID.GoldBar);
                     recipe.AddIngredient(ItemID.GoldBar, 16);
+                }
+
+                //Iron Armor
+
+                if(recipe.TryGetResult(ItemID.IronHelmet, out Item IronHelmet))
+                {
+                    recipe.RemoveIngredient(ItemID.IronBar);
+                    recipe.AddIngredient(ItemID.IronBar, 10);
+                }
+                if(recipe.TryGetResult(ItemID.IronChainmail, out Item IronChainmail))
+                {
+                    recipe.RemoveIngredient(ItemID.IronBar);
+                    recipe.AddIngredient(ItemID.IronBar, 20);
+                }
+                if(recipe.TryGetResult(ItemID.IronGreaves, out Item IronGreaves))
+                {
+                    recipe.RemoveIngredient(ItemID.IronBar);
+                    recipe.AddIngredient(ItemID.IronBar, 10);
+                }
+
+                //Lead Armor
+
+                if(recipe.TryGetResult(ItemID.LeadHelmet, out Item LeadHelmet))
+                {
+                    recipe.RemoveIngredient(ItemID.LeadBar);
+                    recipe.AddIngredient(ItemID.LeadBar, 10);
+                }
+                if(recipe.TryGetResult(ItemID.LeadChainmail, out Item LeadChainmail))
+                {
+                    recipe.RemoveIngredient(ItemID.LeadBar);
+                    recipe.AddIngredient(ItemID.LeadBar, 20);
+                }
+                if(recipe.TryGetResult(ItemID.LeadGreaves, out Item LeadGreaves))
+                {
+                    recipe.RemoveIngredient(ItemID.LeadBar);
+                    recipe.AddIngredient(ItemID.LeadBar, 10);
+                }
+
+                //Silver Armor
+
+                if(recipe.TryGetResult(ItemID.SilverHelmet, out Item SilverHelmet))
+                {
+                    recipe.RemoveIngredient(ItemID.SilverBar);
+                    recipe.AddIngredient(ItemID.SilverBar, 10);
+                }
+                if(recipe.TryGetResult(ItemID.SilverChainmail, out Item SilverChainmail))
+                {
+                    recipe.RemoveIngredient(ItemID.SilverBar);
+                    recipe.AddIngredient(ItemID.SilverBar, 20);
+                }
+                if(recipe.TryGetResult(ItemID.SilverGreaves, out Item SilverGreaves))
+                {
+                    recipe.RemoveIngredient(ItemID.SilverBar);
+                    recipe.AddIngredient(ItemID.SilverBar, 10);
+                }
+
+                //Tungsten Armor
+
+                if(recipe.TryGetResult(ItemID.TungstenHelmet, out Item TungstenHelmet))
+                {
+                    recipe.RemoveIngredient(ItemID.TungstenBar);
+                    recipe.AddIngredient(ItemID.TungstenBar, 10);
+                }
+                if(recipe.TryGetResult(ItemID.TungstenChainmail, out Item TungstenChainmail))
+                {
+                    recipe.RemoveIngredient(ItemID.TungstenBar);
+                    recipe.AddIngredient(ItemID.TungstenBar, 20);
+                }
+                if(recipe.TryGetResult(ItemID.TungstenGreaves, out Item TungstenGreaves))
+                {
+                    recipe.RemoveIngredient(ItemID.TungstenBar);
+                    recipe.AddIngredient(ItemID.TungstenBar, 10);
+                }
+
+                //Copper Armor
+
+                if(recipe.TryGetResult(ItemID.CopperHelmet, out Item CopperHelmet))
+                {
+                    recipe.RemoveIngredient(ItemID.CopperBar);
+                    recipe.AddIngredient(ItemID.CopperBar, 8);
+                }
+                if(recipe.TryGetResult(ItemID.CopperChainmail, out Item CopperChainmail))
+                {
+                    recipe.RemoveIngredient(ItemID.CopperBar);
+                    recipe.AddIngredient(ItemID.CopperBar, 14);
+                }
+                if(recipe.TryGetResult(ItemID.CopperGreaves, out Item CopperGreaves))
+                {
+                    recipe.RemoveIngredient(ItemID.CopperBar);
+                    recipe.AddIngredient(ItemID.CopperBar, 8);
+                }
+
+                //Tin Armor
+
+                if(recipe.TryGetResult(ItemID.TinHelmet, out Item TinHelmet))
+                {
+                    recipe.RemoveIngredient(ItemID.TinBar);
+                    recipe.AddIngredient(ItemID.TinBar, 8);
+                }
+                if(recipe.TryGetResult(ItemID.TinChainmail, out Item TinChainmail))
+                {
+                    recipe.RemoveIngredient(ItemID.TinBar);
+                    recipe.AddIngredient(ItemID.TinBar, 14);
+                }
+                if(recipe.TryGetResult(ItemID.TinGreaves, out Item TinGreaves))
+                {
+                    recipe.RemoveIngredient(ItemID.TinBar);
+                    recipe.AddIngredient(ItemID.TinBar, 8);
                 }
             }
         }
