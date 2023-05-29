3646380ff685fdda22d41abd1322fb57a869e01d
LukasV-Coding <lukas@lukasvogl.de>
2023-05-29 23:15:54 +0200

    4 new Infusions
    Trolly

---
+++ Content/Prefixes/Infusions/InvisibilityInfusion.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+using Terraria.Localization;
+using Terraria;
+using Terraria.ID;
+
+namespace WeDoALittleTrolling.Content.Prefixes
+{
+    public class InvisibilityInfusion : ModPrefix
+    {
+        public override PrefixCategory Category => PrefixCategory.Accessory;
+        public override float RollChance(Item item)
+        {
+            return 0.1f;
+        }
+
+        public override bool CanRoll(Item item)
+        {
+            return true;
+        }
+
+        public override void ModifyValue(ref float valueMult)
+        {
+            valueMult *= 1f + 0.05f;
+        }
+
+        public override void ApplyAccessoryEffects(Player player)
+        {
+            player.AddBuff(BuffID.Invisibility, 1, true, false);
+        }
+
+        public LocalizedText AdditionalTooltip => this.GetLocalization(nameof(AdditionalTooltip));
+
+        public override IEnumerable<TooltipLine> GetTooltipLines(Item item)
+        {
+            yield return new TooltipLine(Mod, "PrefixAcessoryInvisibilityInfusionDescription", AdditionalTooltip.Value) {
+				IsModifier = true,
+			};
+        }
+
+        public override void SetStaticDefaults() {
+            _ = AdditionalTooltip;
+        }
+    }
+
+}
