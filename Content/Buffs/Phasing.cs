c4eb5fefb10291414e35c86a0d898bd5c4eb66f4
LukasV-Coding <lukas@lukasvogl.de>
2023-07-13 21:56:01 +0200

    Began work on phasing
    The problem will be making the details of this one come to life:
    
    -It should work like potion sickness, meaning it will block the usage of other healing items and itself during it's effect.
    -Making the related item useable with the quick heal button but not causing it to inflict vanilla potion sickness
    -The dodge chance will have to be handled in a similar fashion to the searing inferno effect's damage nerf but instead using a hook that modifies the player getting hit
    
    => oh boy this will be a lot of effort

---
+++ Content/Buffs/Phasing.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Terraria;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.GameContent.Creative;
+using WeDoALittleTrolling.Content.NPCs;
+
+
+namespace WeDoALittleTrolling.Content.Buffs
+{
+    public class Phasing : ModBuff
+    {
+
+        public static float dodgeChance = 0.2f;
+
+        public override void SetStaticDefaults()
+        {
+            Main.debuff[Type] = true;
+            BuffID.Sets.LongerExpertDebuff[Type] = false;
+            BuffID.Sets.NurseCannotRemoveDebuff[Type] = true;
+        }
+
+        public override void Update(Player player, ref int buffIndex)
+        {
+            player.moveSpeed *= 1.2f;            
+        }
+    }
+}
