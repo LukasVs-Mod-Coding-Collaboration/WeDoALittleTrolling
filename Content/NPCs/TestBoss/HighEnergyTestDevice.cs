4435fd9450e9a5cef91891ef3e36ab0500dcad7e
LukasV-Coding <lukas@lukasvogl.de>
2023-09-26 21:46:20 +0200

    Test Boss Prep
    Actual enemy and code tomorrow or something. Just taking the first steps to prepare for today. The base texture is done, although the boss is lacking all kinds of attack textures and a shield texture - I will add these later. Regarding the music I intend to use, it's a slightly modified version of a royalty free soundtrack by Kevin MacLeod. All credit to him.
    
    I'll be honest this seems like a colossal task and I'll probably add attacks as I go and add more functionality to the best of my ability.

---
+++ Content/NPCs/TestBoss/HighEnergyTestDevice.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeDoALittleTrolling.Content.NPCs.TestBoss
+{
+    internal class HighEnergyTestDevice
+    {
+    }
+}
