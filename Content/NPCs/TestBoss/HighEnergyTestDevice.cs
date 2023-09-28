f93bc0413f033482bd9bb8193fbc3923d6966f60
LukasV-Coding <lukas@lukasvogl.de>
2023-09-28 16:34:14 +0200

    This is gonna take a while
    trying to get the boss to always target the nearest player with the highest aggro will take a while even with the NPC arrays.

--- Content/NPCs/TestBoss/HighEnergyTestDevice.cs
+++ Content/NPCs/TestBoss/HighEnergyTestDevice.cs
@@ -1,12 +1,60 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Xna.Framework;
+using System;
+using Terraria;
+using Terraria.Utilities;
+using Terraria.ID;
+using Terraria.ModLoader;
+using WeDoALittleTrolling.Content.Buffs;
+using Terraria.Audio;
+using WeDoALittleTrolling.Common.Utilities;
+using Terraria.DataStructures;
 
 namespace WeDoALittleTrolling.Content.NPCs.TestBoss
 {
-    internal class HighEnergyTestDevice
+    public class HighEnergyTestDevice : ModNPC
     {
+
+        private int bossPhase = 0;
+        public override void SetDefaults()
+        {
+            NPC.width = 246;
+            NPC.height = 246;
+            NPC.damage = 14;
+            NPC.defense = 20;
+            NPC.lifeMax = 250000;
+            NPC.HitSound = SoundID.NPCHit53;
+            NPC.DeathSound = SoundID.NPCDeath56;
+            NPC.value = 10000000f;
+            NPC.knockBackResist = 0.0f;
+            NPC.aiStyle = -1;
+            NPC.noTileCollide = true;
+            Music = MusicLoader.GetMusicSlot(Mod, "Assets/Music/TestBossTheme");
+            NPC.EncourageDespawn(10);
+
+        }
+
+        public override void OnSpawn(IEntitySource source)
+        {
+            bossPhase = 1;
+            base.OnSpawn(source);
+        }
+
+        public override void AI()
+        {
+            AI_TestBoss_High_Energy_Test_Device();
+        }
+
+        private void AI_TestBoss_High_Energy_Test_Device()
+        {
+            if (bossPhase == 0)
+            {
+                NPC.velocity = new Vector2(0f, 0.4f);
+            }
+        }
+
+
+
+
+
     }
 }
