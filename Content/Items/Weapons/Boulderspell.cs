99e0c9ed22c2b2b5d2537085bed3dd80d2bef16c
LukasV-Coding <lukas@lukasvogl.de>
2023-10-17 19:15:14 +0200

    First Boulderspell Patch
    Yes, It's broken, in more ways than one, but this is just the foundation dw

---
+++ Content/Items/Weapons/Boulderspell.cs
@@ -0,0 +1,113 @@
+﻿/*
+    WeDoALittleTrolling is a Terraria Mod made with tModLoader.
+    Copyright (C) 2022-2023 LukasV-Coding
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+using Terraria;
+using Terraria.Utilities;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Microsoft.Xna.Framework;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.GameContent.Creative;
+using static Humanizer.In;
+using static Terraria.ModLoader.PlayerDrawLayer;
+using System;
+using Microsoft.CodeAnalysis;
+using WeDoALittleTrolling.Common.ModPlayers;
+
+namespace WeDoALittleTrolling.Content.Items.Weapons
+{
+    internal class Boulderspell : ModItem
+    {
+        public static UnifiedRandom rnd = new UnifiedRandom(); //Introduce random Values
+
+        public override void SetDefaults()
+        {
+            Item.width = 28;
+            Item.height = 30;
+
+            Item.consumable = false;
+
+            Item.value = Item.sellPrice(gold: 3);
+            Item.maxStack = 1;
+
+            Item.autoReuse = false;
+            Item.useTime = 45;
+            Item.useAnimation = 45;
+            Item.mana = 50;
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.useTurn = false;
+            Item.UseSound = SoundID.Item69;
+
+            Item.damage = 140;
+            Item.DamageType = DamageClass.Default;
+            Item.knockBack = 8f;
+
+            Item.rare = ItemRarityID.Red;
+            Item.scale = 1.0f;
+
+            Item.shoot = ProjectileID.Boulder;
+            Item.shootSpeed = 8f;
+        }
+
+        public override bool AltFunctionUse(Player player) // Boulder Rain
+        {
+            return false;
+        }
+
+
+        public override void ModifyWeaponDamage(Player player, ref StatModifier damage)
+        {
+            if (Main.expertMode)
+            {
+                damage *= 2;
+            }
+            if (Main.expertMode)
+            {
+                damage *= 1.5f;
+            }
+            base.ModifyWeaponDamage(player, ref damage);
+        }
+
+        public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
+        {
+            //Vector2 testspawnpos = new Vector2(Main.MouseWorld.X, Main.MouseWorld.Y);
+            //this works
+
+            //Vector2 testmomentumdown = new Vector2(Main.MouseWorld.X, Main.MouseWorld.Y + 20);
+            //This does not work, the boulder just explodes.
+
+            //position = testspawnpos;
+            //velocity = testmomentumdown;
+
+
+
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+              .AddTile(TileID.CrystalBall)
+              .AddIngredient(ItemID.SpellTome, 1)
+              .AddIngredient(ItemID.Boulder, 50)
+              .AddIngredient(ItemID.SoulofLight, 10)
+              .AddIngredient(ItemID.SoulofNight, 10)
+              .Register();
+        }
+    }
+}
