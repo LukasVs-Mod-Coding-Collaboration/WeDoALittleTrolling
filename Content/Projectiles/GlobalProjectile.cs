7c11eb99d3da45c212e5e4bb5a14ff680f8cddf4
DarkWav <simon.vogl@gmx.net>
2023-11-01 23:41:49 +0100

    Make Flares only last 30 seconds...

--- Content/Projectiles/GlobalProjectile.cs
+++ Content/Projectiles/GlobalProjectile.cs
@@ -145,6 +145,18 @@ namespace WeDoALittleTrolling.Content.Projectiles
         {
             if
             (
+                projectile.type == ProjectileID.Flare ||
+                projectile.type == ProjectileID.BlueFlare ||
+                projectile.type == ProjectileID.SpelunkerFlare ||
+                projectile.type == ProjectileID.CursedFlare ||
+                projectile.type == ProjectileID.RainbowFlare ||
+                projectile.type == ProjectileID.ShimmerFlare
+            )
+            {
+                projectile.timeLeft = 1800;
+            }
+            if
+            (
                 projectile.type == ProjectileID.InfluxWaver || projectile.type == ProjectileID.FlamingJack
             )
             {
