d6a0dc07e8d6917f7378001c13de60b0363b40bc
LukasV-Coding <lukas@lukasvogl.de>
2024-04-29 16:16:02 +0200

    Push Eater of Sticks

---
+++ Content/Items/Weapons/EaterOfSticks.cs
@@ -0,0 +1,68 @@
+﻿/*
+    WeDoALittleTrolling is a Terraria Mod made with tModLoader.
+    Copyright (C) 2022-2024 LukasV-Coding
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+using Terraria;
+using Terraria.Utilities;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Microsoft.Xna.Framework;
+using Terraria.Audio;
+using System;
+using WeDoALittleTrolling.Common.ModPlayers;
+
+namespace WeDoALittleTrolling.Content.Items.Weapons
+{
+    internal class EaterOfSticks : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.width = 40;
+            Item.height = 40;
+
+            Item.consumable = false;
+
+            Item.value = Item.sellPrice(silver: 50);
+            Item.maxStack = 1;
+
+            Item.autoReuse = true;
+            Item.useTime = 20;
+            Item.useAnimation = 20;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.useTurn = true;
+            Item.UseSound = SoundID.Item1;
+
+            Item.damage = 19;
+            Item.DamageType = DamageClass.Melee;
+            Item.knockBack = 7f;
+            Item.crit = 6;
+
+            Item.rare = ItemRarityID.Blue;
+            Item.scale = 1.25f;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+              .AddTile(TileID.Anvils)
+              .AddIngredient(ItemID.Shadewood, 5)
+              .AddIngredient(ItemID.RottenChunk, 10)
+              .AddIngredient(ItemID.WormTooth, 2)
+              .Register();
+        }
+    }
+}
