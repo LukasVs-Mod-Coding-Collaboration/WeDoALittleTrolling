91c2cd0bec2aa64421f0356d18be8f37d095cbfd
DarkWav <simon.vogl@gmx.net>
2024-10-27 01:50:23 +0200

    Add Accurate Reforge for Classless Items

---
+++ Content/Prefixes/Accurate.cs
@@ -0,0 +1,62 @@
+﻿/*
+    WeDoALittleTrolling is a Terraria Mod made with tModLoader.
+    Copyright (C) 2022-2024 LukasV-Coding
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+using Terraria.Localization;
+using Terraria;
+
+namespace WeDoALittleTrolling.Content.Prefixes
+{
+    public class Accurate : ModPrefix
+    {
+        public override PrefixCategory Category => PrefixCategory.AnyWeapon;
+        public override float RollChance(Item item)
+        {
+            return 1.0f;
+        }
+
+        public override bool CanRoll(Item item)
+        {
+            if (item.DamageType == DamageClass.Generic)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override void ModifyValue(ref float valueMult)
+        {
+            valueMult *= 1.4f;
+        }
+
+        public override void SetStats(ref float damageMult, ref float knockbackMult, ref float useTimeMult, ref float scaleMult, ref float shootSpeedMult, ref float manaMult, ref int critBonus)
+        {
+            critBonus += 15;
+        }
+
+    }
+
+}
