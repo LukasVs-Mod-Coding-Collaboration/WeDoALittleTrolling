fafdc19709b6ac01f41aed67c436be8745b0eab9
DarkWav <simon.vogl@gmx.net>
2024-12-01 18:58:01 +0100

    Disable Minute Reforge

--- Content/Prefixes/Minute.cs
+++ Content/Prefixes/Minute.cs
@@ -15,6 +15,7 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
+/*
 
 using Terraria;
 using Terraria.ModLoader;
@@ -82,3 +83,4 @@ namespace WeDoALittleTrolling.Content.Prefixes
         }
     }
 }
+*/
