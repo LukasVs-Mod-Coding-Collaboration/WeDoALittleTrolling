9b06940db021ff79656dd6f2c187fd230a25d33b
DarkWav <simon.vogl@gmx.net>
2024-12-18 22:30:13 +0100

    Remove WDALQOL Embedding, spin off WDALQOL into a fully independent mod.

--- Common/ModSystems/WDALTIntermediateLanguageEditing.cs
+++ Common/ModSystems/WDALTIntermediateLanguageEditing.cs
@@ -30,47 +30,14 @@ namespace WeDoALittleTrolling.Common.ModSystems
     {
         public static void RegisterILHooks()
         {
-            IL_WorldGen.UpdateWorld_Inner += IL_WorldGen_UpdateWorld;
             IL_NPC.AI_037_Destroyer += IL_NPC_AI_037_Destroyer;
-            IL_Player.UpdateBiomes += IL_Player_UpdateBiomes;
             IL_Player.Update_NPCCollision += IL_Player_Update_NPCCollision;
-            IL_Main.UpdateTime_SpawnTownNPCs += IL_Main_UpdateTime_SpawnTownNPCs;
         }
 
         public static void UnregisterILHooks()
         {
-            IL_WorldGen.UpdateWorld_Inner -= IL_WorldGen_UpdateWorld;
             IL_NPC.AI_037_Destroyer -= IL_NPC_AI_037_Destroyer;
-            IL_Player.UpdateBiomes -= IL_Player_UpdateBiomes;
             IL_Player.Update_NPCCollision -= IL_Player_Update_NPCCollision;
-            IL_Main.UpdateTime_SpawnTownNPCs -= IL_Main_UpdateTime_SpawnTownNPCs;
-        }
-
-        public static void IL_WorldGen_UpdateWorld(ILContext intermediateLanguageContext)
-        {
-            bool successInjectInfectionSpreadHook = true;
-            try
-            {
-                ILCursor cursor = new ILCursor(intermediateLanguageContext);
-                cursor.GotoNext(i => i.MatchStsfld<WorldGen>(nameof(WorldGen.AllowedToSpreadInfections))); //Go to the place right after the "AllowedToSpreadInfections" variable is set.
-                cursor.Index++; //Go in front of it now.
-                cursor.Emit(OpCodes.Ldc_I4_0); //set "false" as the parameter to write.
-                cursor.Emit(OpCodes.Stsfld, typeof(WorldGen).GetField(nameof(WorldGen.AllowedToSpreadInfections))); //Write "false" into the "AllowedToSpreadInfections" variable.
-                cursor.GotoNext(i => i.MatchStsfld<WorldGen>(nameof(WorldGen.AllowedToSpreadInfections))); //Do the same if StopBiomeSpreadPower is enabled.
-                cursor.Index++;
-                cursor.Emit(OpCodes.Ldc_I4_0);
-                cursor.Emit(OpCodes.Stsfld, typeof(WorldGen).GetField(nameof(WorldGen.AllowedToSpreadInfections)));
-            }
-            catch
-            {
-                MonoModHooks.DumpIL(ModContent.GetInstance<WeDoALittleTrolling>(), intermediateLanguageContext);
-                WeDoALittleTrolling.logger.Fatal("WDALT: Failed to inject Infection Spread Hook. Broken IL Code has been dumped to tModLoader-Logs/ILDumps/WeDoALittleTrolling.");
-                successInjectInfectionSpreadHook = false;
-            }
-            if(successInjectInfectionSpreadHook)
-            {
-                WeDoALittleTrolling.logger.Debug("WDALT: Successfully injected Infection Spread Hook via IL Editing.");
-            }
         }
 
         public static void IL_NPC_AI_037_Destroyer(ILContext intermediateLanguageContext)
@@ -166,52 +133,6 @@ namespace WeDoALittleTrolling.Common.ModSystems
             if(successInjectDestroyerAIHook)
             {
                 WeDoALittleTrolling.logger.Debug("WDALT: Successfully injected Destroyer AI Hook via IL Editing.");
-            }
-        }
-
-        public static void IL_Player_UpdateBiomes(ILContext intermediateLanguageContext)
-        {
-            bool successInjectGetGoodWorldLightingHook = true;
-            try
-            {
-                ILCursor cursor = new ILCursor(intermediateLanguageContext);
-                cursor.GotoNext(i => i.MatchLdsfld<Main>(nameof(Main.getGoodWorld)));
-                cursor.Index++; //move cursor to the "Main.getGoodWorld" if statement.
-                cursor.Emit(OpCodes.Pop); //Pop the value of Main.getGoodWorld off the stack.
-                cursor.Emit(OpCodes.Ldc_I4_0); //Push "false" onto the stack. This causes the if statement to never run the code inside.
-            }
-            catch
-            {
-                MonoModHooks.DumpIL(ModContent.GetInstance<WeDoALittleTrolling>(), intermediateLanguageContext);
-                WeDoALittleTrolling.logger.Fatal("WDALT: Failed to inject For The Worthy Lighting Hook. Broken IL Code has been dumped to tModLoader-Logs/ILDumps/WeDoALittleTrolling.");
-                successInjectGetGoodWorldLightingHook = false;
-            }
-            if(successInjectGetGoodWorldLightingHook)
-            {
-                WeDoALittleTrolling.logger.Debug("WDALT: Successfully injected For The Worthy Lighting Hook via IL Editing.");
-            }
-        }
-
-        public static void IL_Main_UpdateTime_SpawnTownNPCs(ILContext intermediateLanguageContext)
-        {
-            bool successInjectTownNPCsRespawnTimeHook = true;
-            try
-            {
-                ILCursor cursor = new ILCursor(intermediateLanguageContext);
-                cursor.GotoNext(i => i.MatchLdcR8(7200.0)); //move cursor towards the town NPC spawn time intervall (7200 / 60 = 120 seconds)
-                cursor.Index++; //move cursor after the town NPC spawn time intervall.
-                cursor.Emit(OpCodes.Pop); //Pop 7200 off the stack.
-                cursor.Emit(OpCodes.Ldc_R8, 900.0); //Push 900 onto the stack. This causes the town NPC spawn time intervall to reduce to 900 / 60 = 15 seconds.
-            }
-            catch
-            {
-                MonoModHooks.DumpIL(ModContent.GetInstance<WeDoALittleTrolling>(), intermediateLanguageContext);
-                WeDoALittleTrolling.logger.Fatal("WDALT: Failed to inject Town NPCs Respawn Time Hook. Broken IL Code has been dumped to tModLoader-Logs/ILDumps/WeDoALittleTrolling.");
-                successInjectTownNPCsRespawnTimeHook = false;
-            }
-            if(successInjectTownNPCsRespawnTimeHook)
-            {
-                WeDoALittleTrolling.logger.Debug("WDALT: Successfully injected Town NPCs Respawn Time Hook via IL Editing.");
             }
         }
 
